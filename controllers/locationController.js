dd16952800a26a37a8f8464405b90b4f18fd303a
Puneeth1525 <88479542+Puneeth1525@users.noreply.github.com>
2024-06-26 18:20:53 -0400

    location

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,7 +1,12 @@
-const Location = require('../models/locationModel');
+const Location = require('../models/locationModel'); // Assuming you have a separate model file
 
+// Function to handle location data saving
 exports.saveLocation = async (req, res) => {
   const { latitude, longitude } = req.body;
+
+  if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+    return res.status(400).send('Latitude and Longitude must be numbers');
+  }
 
   const newLocation = new Location({ latitude, longitude });
 
--- models/locationModel.js
+++ models/locationModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-  latitude: Number,
-  longitude: Number,
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
